0a661d0df38598aac0351d09ed9a4e5834a9ee5c
Marcello Brito <just4animes@gmail.com>
2021-01-09 15:58:24 -0300

    Filtros de Música

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 const {Client, MessageEmbed} = require('discord.js'); const {Player} = require('discord-player')
 const client = new Client({disableMentions: "everyone"}); const player = new Player(client);
+const filters = require('./configs/music/filters.json')
 const {registerCommands, registerEvents, registerPlayerEvents} = require('../src/utils/validation/registry');
 
 (async() => {
-    client.login(process.env.TOKEN); client.player = player; client.commands = new Map();
+    client.login(process.env.TOKEN); client.player = player; client.commands = new Map(); client.filters = filters;
     await registerPlayerEvents(player, '../../events/playerEvents')
     await registerCommands(client, '../../commands');
     await registerEvents(client, '../../events/discordEvents');
